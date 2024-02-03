7a5a70c28f37c58b98c88e497539b711658e2103
Edgard Carrion <edgard.jonthcs@gmail.com>
2024-02-03 13:37:18 -0300

    Desafio js calculando el total

---
+++ bases.js
@@ -0,0 +1,29 @@
+// let precio = parseInt(prompt('Ingresa un precio'))
+let precio =12000
+let precioSpan = document.querySelector(".precio-inicial");
+precioSpan.innerHTML = precio
+
+
+// let precioTotal = document.querySelector('.valor-total')
+// let cantidad = document.querySelector('.cantidad')
+
+
+
+// let valor1 = 0
+// let valor2 = 0
+
+// let botonMas = document.querySelector('.boton-mas')
+// let botonMenos = document.querySelector('.boton-menos')
+// botonMas.addEventListener('click', function () {
+//    valor1 = valor1 + precio
+//    valor2 = valor2 + 1
+//    precioTotal.innerHTML = valor1
+//    cantidad.innerHTML = valor2
+// })
+
+// botonMenos.addEventListener('click', function () {
+//     valor1 = valor1 - precio
+//     valor2 = valor2 - 1
+//     precioTotal.innerHTML = valor1
+//     cantidad.innerHTML = valor2
+// })
